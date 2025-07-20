69083a557cff1772612f18a45125a0ec0c11ba71
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-07-20 17:14:21 +0530

    Add language button

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,22 +1,14 @@
-'use client';
-
 import React from 'react';
 
 import Logo from '@/assets/svg/logo';
-import TranslationIcon from '@/assets/svg/translation-icon';
-import { useLocaleContext } from '@/i18n/localizationWrapper';
-import Button from '../Button/Button';
+import LanguageButton from '../LanguageButton/LanguageButton';
 import classes from './MainHeader.module.scss';
 
 function MainHeader(): React.ReactElement {
-  const { toggleLanguage } = useLocaleContext();
-
   return (
     <header className={classes.container}>
       <Logo />
-      <Button onClick={toggleLanguage}>
-        <TranslationIcon />
-      </Button>
+      <LanguageButton />
     </header>
   );
 }
