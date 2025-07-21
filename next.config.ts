fe7ce3262cde93fa64ae965030a5de4d958fd3aa
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-07-22 00:11:31 +0530

    [refactor] code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  sassOptions: {
+    additionalData: `@use '@/styles/theme.scss' as *;`,
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
