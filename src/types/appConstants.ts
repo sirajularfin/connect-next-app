150c433a2d5260594ea7c01360b11d10b0603643
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-07-23 11:58:27 +0530

    Remove unused AUTH_MODES enum from appConstants

--- src/types/appConstants.ts
+++ src/types/appConstants.ts
@@ -1,13 +1,5 @@
 export const DEFAULT_VALUE_ZERO = 0;
 
-export enum AUTH_MODES {
-  LOGIN = 'login',
-  REGISTER = 'register',
-  FORGOT_PASSWORD = 'forgotPassword',
-  OTP_VERIFICATION = 'otpVerification',
-  RESET_PASSWORD = 'resetPassword',
-}
-
 export enum APP_LANGUAGES {
   EN = 'en',
   HI = 'hi',
