c4dae08c910552d0d6d04cf644ee4856000b8425
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-07-24 19:59:53 +0530

    remove unused var

--- src/integrations/http/networkLoadingEndpoints.ts
+++ src/integrations/http/networkLoadingEndpoints.ts
@@ -1,15 +1,11 @@
 import { authApiEndpoints } from './endpoints/auth';
 import { tokenApiEndpoints } from './endpoints/token';
-import { userApiEndpoints } from './endpoints/user';
 
 const NetworkLoadingEndpoints = [
   authApiEndpoints.getLogout,
   authApiEndpoints.postLogin,
   authApiEndpoints.postRegistration,
   tokenApiEndpoints.postRefreshToken,
-  userApiEndpoints.getActiveUserProfile,
-  userApiEndpoints.getUsers,
-  userApiEndpoints.getUserById,
 ];
 
 export default NetworkLoadingEndpoints;
