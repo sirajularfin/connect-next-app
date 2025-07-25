890716504d239aa8769a2c5f608e794125e34738
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-07-25 13:02:28 +0530

    update headers

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,28 @@ const nextConfig: NextConfig = {
   sassOptions: {
     additionalData: `@use '@/styles/theme.scss' as *;`,
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.NEXT_PUBLIC_SITE_URL,
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        ],
+      },
+    ];
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
