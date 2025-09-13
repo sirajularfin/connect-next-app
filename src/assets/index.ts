344c0a39f2269433945539f28cdd33448530ca21
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-09-13 23:47:38 +0530

    complete profile header

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,15 @@
+import LandingImage from '@/assets/images/landing-page.png';
+import ProfilePlaceholder from '@/assets/images/profile-image.png';
 import GoogleLogo from '@/assets/svg/google-logo.svg';
 import LanguageIcon from '@/assets/svg/language-icon.svg';
 import Logo from '@/assets/svg/logo.svg';
 import SearchIcon from '@/assets/svg/search-icon.svg';
 
-export { GoogleLogo, LanguageIcon, Logo, SearchIcon };
+export {
+  GoogleLogo,
+  LandingImage,
+  LanguageIcon,
+  Logo,
+  ProfilePlaceholder,
+  SearchIcon,
+};
