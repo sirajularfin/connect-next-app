6d8df07f640920fa865443552f47ba2369055d32
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-09-15 15:16:44 +0530

    add send button

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,6 +4,7 @@ import AttachmentIcon from '@/assets/svg/attachment-icon.svg';
 import GoogleLogo from '@/assets/svg/google-logo.svg';
 import LanguageIcon from '@/assets/svg/language-icon.svg';
 import Logo from '@/assets/svg/logo.svg';
+import RightArrowIcon from '@/assets/svg/right-arrow.svg';
 import SearchIcon from '@/assets/svg/search-icon.svg';
 
 export {
@@ -13,5 +14,7 @@ export {
   LanguageIcon,
   Logo,
   ProfilePlaceholder,
+  RightArrowIcon,
   SearchIcon,
 };
+
--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,7 +1,8 @@
 import { useTranslations } from 'next-intl';
 import React from 'react';
 
-import { AttachmentIcon } from '../../assets';
+import { AttachmentIcon, RightArrowIcon } from '../../assets';
+import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 import classes from './ChatInput.module.scss';
 
@@ -12,6 +13,12 @@ interface IProps {
 
 const ChatInput: React.FC<IProps> = React.memo(({ value, onChange }) => {
   const t = useTranslations();
+
+  const enterButton = () => (
+    <Button type="submit" variant="PRIMARY" className={classes.sendButton}>
+      <RightArrowIcon />
+    </Button>
+  );
 
   return (
     <div className={classes.container}>
@@ -24,6 +31,7 @@ const ChatInput: React.FC<IProps> = React.memo(({ value, onChange }) => {
         customStyle={classes.textInput}
         value={value}
         onChange={e => onChange(e.target.value)}
+        endIcon={enterButton()}
       />
     </div>
   );
