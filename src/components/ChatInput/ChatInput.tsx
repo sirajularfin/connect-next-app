dcb3f5afbbaedc612146b39ebfbec9e98669606c
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-09-15 15:22:59 +0530

    memoize chat input component

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,9 +1,9 @@
 import { useTranslations } from 'next-intl';
 import React from 'react';
 
-import { AttachmentIcon, RightArrowIcon } from '../../assets';
-import Button from '../Button/Button';
-import TextInput from '../TextInput/TextInput';
+import { AttachmentIcon, RightArrowIcon } from '@/assets';
+import Button from '@/components/Button/Button';
+import TextInput from '@/components/TextInput/TextInput';
 import classes from './ChatInput.module.scss';
 
 interface IProps {
@@ -14,10 +14,20 @@ interface IProps {
 const ChatInput: React.FC<IProps> = React.memo(({ value, onChange }) => {
   const t = useTranslations();
 
-  const enterButton = () => (
-    <Button type="submit" variant="PRIMARY" className={classes.sendButton}>
-      <RightArrowIcon />
-    </Button>
+  const enterButton = React.useCallback(
+    () => (
+      <Button type="submit" variant="PRIMARY" className={classes.sendButton}>
+        <RightArrowIcon />
+      </Button>
+    ),
+    []
+  );
+
+  const handleChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      onChange(e.target.value);
+    },
+    []
   );
 
   return (
@@ -30,7 +40,7 @@ const ChatInput: React.FC<IProps> = React.memo(({ value, onChange }) => {
         placeholder={t('chat_input_placeholder')}
         customStyle={classes.textInput}
         value={value}
-        onChange={e => onChange(e.target.value)}
+        onChange={handleChange}
         endIcon={enterButton()}
       />
     </div>
