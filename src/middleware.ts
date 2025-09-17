5b80b199ea59b4b063dccc337460422d76f3dabd
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-09-18 00:05:55 +0530

    feat: update enum in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
+
 import { APP_ROUTES } from './common/types/appRoutes.type';
 
 export async function middleware(request: NextRequest) {
@@ -11,33 +12,23 @@ export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Protected routes
-  if (pathname.includes(APP_ROUTES.PROTECTED.CHAT)) {
+  if (pathname.includes(APP_ROUTES.CHAT)) {
     if (!token) {
-      return NextResponse.redirect(
-        new URL(APP_ROUTES.PUBLIC.LOGIN, request.url)
-      );
+      return NextResponse.redirect(new URL(APP_ROUTES.LOGIN, request.url));
     }
   }
 
   // Redirect authenticated users from login page
   if (
-    (pathname === APP_ROUTES.PUBLIC.LOGIN ||
-      pathname === APP_ROUTES.PUBLIC.REGISTER) &&
+    (pathname === APP_ROUTES.LOGIN || pathname === APP_ROUTES.REGISTER) &&
     token
   ) {
-    return NextResponse.redirect(
-      new URL(APP_ROUTES.PROTECTED.CHAT, request.url)
-    );
+    return NextResponse.redirect(new URL(APP_ROUTES.CHAT, request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    '/chat/:path*',
-    '/profile/:path*',
-    '/login',
-    '/api/protected/:path*',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
