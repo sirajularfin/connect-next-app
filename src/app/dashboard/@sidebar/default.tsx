cf0c486e2ff8656ce4c136ced4eeb329d9998a2b
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-09-19 01:16:53 +0530

    feat: add sidebar

---
+++ src/app/dashboard/@sidebar/default.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './sidebar.module.scss';
+
+const Sidebar: React.FC = () => {
+  return <div className={classes.container}>Sidebar Content</div>;
+};
+
+export default Sidebar;
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,21 +3,24 @@ import React from 'react';
 import { ProfileProvider } from '@/context/ProfileContext';
 import { API_URLS } from '@/network/apiConstants.type';
 import { httpClient } from '@/network/httpClient';
+import classes from './dashboard.module.scss';
 
 interface IProps {
+  sidebar: React.ReactNode;
   children: React.ReactNode;
 }
 
-const loadUserProfile = async () => {
-  const { data } = await httpClient.get(API_URLS.USER_PROFILE.getProfile);
-  return data;
-};
+const Layout: React.FC<IProps> = async ({ sidebar, children }) => {
+  const { data: initialProfile } = await httpClient.get(
+    API_URLS.USER_PROFILE.getProfile
+  );
 
-const Layout: React.FC<IProps> = async ({ children }) => {
-  const initialProfile = await loadUserProfile();
   return (
     <ProfileProvider initialProfile={initialProfile}>
-      {children}
+      <div className={classes.dashboardLayout}>
+        <section className={classes.dashboardSidebar}>{sidebar}</section>
+        <section>{children}</section>
+      </div>
     </ProfileProvider>
   );
 };
